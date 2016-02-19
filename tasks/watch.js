9d60403b3fe383c72649d6ec5d404de4fd5706b8
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-02-19 17:19:02 -0600

    Check flow before running tests

--- tasks/watch.js
+++ tasks/watch.js
@@ -13,10 +13,10 @@ module.exports = {
       '.flowconfig',
     ],
     tasks: [
+      'flowbin:status',
       'build-tests',
       'mochaTest',
       'lint',
-      'flowbin:status',
     ],
   },
 };
