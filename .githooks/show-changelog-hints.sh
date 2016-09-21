b54ec565dd5e0c2053af684b0ed69c3b16f09e99
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-09-21 15:53:31 -0500

    chore: Add helper

--- .githooks/show-changelog-hints.sh
+++ .githooks/show-changelog-hints.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# This shows some hints after one of the git commit hooks for checking
+# changelog messages fails.
+#
+# Because commits are hard to remove (unless you are a pro at interactive
+# rebasing), these commit hook checks should make it easier for new
+# contributors.
 echo
 echo "-------------------------------------------------------------------"
 echo "The above output indicates a problem with one or more of your "
