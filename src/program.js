1a47a4318b7d9e43e22f7761c6b82d5a9d0e2f2e
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-11-08 14:14:44 -0600

    refactor: Make the help text more accurate for --warnings-as-errors

--- src/program.js
+++ src/program.js
@@ -301,7 +301,7 @@ Example: $0 --help run.
         default: false,
       },
       'warnings-as-errors': {
-        describe: 'Treat warnings as errors',
+        describe: 'Exit non-zero if there are warnings',
         alias: 'w',
         type: 'boolean',
         default: false,
