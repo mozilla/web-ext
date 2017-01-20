3c7a2c7cae90a589b7d9ad6dbc9fe1f82ef60c84
Luca Greco <luca.greco@alcacoop.it>
2017-01-20 16:17:32 +0100

    fix: Skip test on git-rev-sync from test.program.js unit test on Windows

--- tests/unit/test.program.js
+++ tests/unit/test.program.js
@@ -10,7 +10,7 @@ import {assert} from 'chai';
 import {defaultVersionGetter, main, Program} from '../../src/program';
 import commands from '../../src/cmd';
 import {onlyInstancesOf, UsageError} from '../../src/errors';
-import {fake, makeSureItFails} from './helpers';
+import {fake, makeSureItFails, it_skipOnWin32} from './helpers';
 import {ConsoleStream} from '../../src/util/logger';
 
 
@@ -434,11 +434,10 @@ describe('program.defaultVersionGetter', () => {
       });
   });
 
-  it('returns git commit information in development', () => {
+  it_skipOnWin32('returns git commit information in development', () => {
     const commit = `${git.branch()}-${git.long()}`;
     const testBuildEnv = {globalEnv: 'development'};
     assert.equal(defaultVersionGetter(root, testBuildEnv),
                  commit);
   });
-
 });
