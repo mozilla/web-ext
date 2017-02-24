07a267d7536b2a8ad86402c28002ce267fb51bf3
Stseb <tilitidam@gmail.com>
2017-02-24 20:17:29 +0100

    feat: dafault and optional linter config

--- src/util/linter.js
+++ src/util/linter.js
@@ -16,31 +16,53 @@ export type linterParams = {|
   ignoreFiles?: Array<string>,
 |};
 
+
+export type linterConfig = {|
+  logLevel?: string,
+  stack?: boolean,
+  pretty?: boolean,
+  warningsAsErrors?: boolean,
+  metadata?: boolean,
+  scanFile?: string | null,
+  shouldScanFile?: Function | null,
+|};
+
+const defaultConfig = {
+  logLevel: 'fatal',
+  stack: true,
+  pretty: true,
+  warningsAsErrors: false,
+  metadata: false,
+  scanFile: null,
+  shouldScanFile: null,
+};
+
 export async function linter(
   {
     sourceDir, artifactsDir, ignoreFiles = [], filePath = null,
     createLinter = defaultLinterCreator,
     fileFilterCreator = defaultFileFilterCreator,
-  }: linterParams
+  }: linterParams,
+  additionalConfig: linterConfig = {},
 ): Promise<void> {
-  const relativePath = filePath ? [path.relative(sourceDir, filePath)]
+  const config = {
+    ...defaultConfig,
+    ...additionalConfig,
+  };
+
+  config._ = [sourceDir];
+
+  config.scanFile = filePath ? [path.relative(sourceDir, filePath)]
     : filePath;
-  const fileFilter = fileFilterCreator({
-    sourceDir, artifactsDir, ignoreFiles,
-  });
+  if (!filePath) {
+    const fileFilter = fileFilterCreator({
+      sourceDir, artifactsDir, ignoreFiles,
+    });
+    config.shouldScanFile = (fileName) => fileFilter.wantFile(fileName);
+  }
+
   const linterInstance = createLinter({
-    config: {
-      logLevel: 'fatal',
-      stack: true,
-      pretty: true,
-      warningsAsErrors: false,
-      metadata: false,
-      scanFile: relativePath,
-      shouldScanFile: (fileName) => fileFilter.wantFile(fileName),
-      // This mimics the first command line argument from yargs,
-      // which should be the directory to the extension.
-      _: [sourceDir],
-    },
+    config,
     runAsBinary: false,
   });
   return linterInstance.run();
