5200ea81f311afd92828c4703393fa7603cd890e
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-08-03 15:53:02 -0500

    Expose util.logger

--- tests/unit/test.web-ext.js
+++ tests/unit/test.web-ext.js
@@ -6,6 +6,7 @@ import webExt from '../../src/main';
 import build from '../../src/cmd/build';
 import run from '../../src/cmd/run';
 import {main} from '../../src/program';
+import {consoleStream} from '../../src/util/logger';
 
 
 describe('webExt', () => {
@@ -17,5 +18,9 @@ describe('webExt', () => {
     // This just checks a sample of commands.
     assert.equal(webExt.cmd.run, run);
     assert.equal(webExt.cmd.build, build);
+  });
+
+  it('gives you access to the log stream', () => {
+    assert.equal(webExt.util.logger.consoleStream, consoleStream);
   });
 });
