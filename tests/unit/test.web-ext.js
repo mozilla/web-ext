a9abcc14ebe1a0b0ea361705080ee3e6e7c0fa84
Rob Wu <rob@robwu.nl>
2018-04-12 16:14:02 +0200

    test: refactor failing test and improve coverage
    A previous commit changed `src/cmd/index.js` to not directly export the
    implementations, but to lazy-load those modules.
    So the test that did a simple comparison started failing.
    
    To fix this test and maintain test coverage, change the test to
    temporarily intercept invocations of the original cmd module,
    for every command that is exported in `src/cmd/index.js`.

--- tests/unit/test.web-ext.js
+++ tests/unit/test.web-ext.js
@@ -1,10 +1,9 @@
 /* @flow */
-import {describe, it} from 'mocha';
+import {afterEach, describe, it} from 'mocha';
 import {assert} from 'chai';
+import sinon from 'sinon';
 
 import webExt from '../../src/main';
-import build from '../../src/cmd/build';
-import run from '../../src/cmd/run';
 import {main} from '../../src/program';
 import {consoleStream} from '../../src/util/logger';
 
@@ -14,13 +13,36 @@ describe('webExt', () => {
     assert.equal(webExt.main, main);
   });
 
-  it('exposes commands', () => {
-    // This just checks a sample of commands.
-    assert.equal(webExt.cmd.run, run);
-    assert.equal(webExt.cmd.build, build);
-  });
-
   it('gives you access to the log stream', () => {
     assert.equal(webExt.util.logger.consoleStream, consoleStream);
+  });
+
+  describe('exposes commands', () => {
+    let stub: sinon.Stub;
+    afterEach(() => {
+      stub.restore();
+      stub = undefined;
+    });
+    for (const cmd of ['run', 'lint', 'build', 'sign', 'docs']) {
+      it(`lazily loads cmd/${cmd}`, async () => {
+        // TODO: use async import instead of require - https://github.com/mozilla/web-ext/issues/1306
+        // $FLOW_IGNORE: non-literal require used only in tests.
+        const cmdModule = require(`../../src/cmd/${cmd}`);
+        stub = sinon.stub(cmdModule, 'default');
+
+        const params = {};
+        const options = {};
+        const expectedResult = {};
+        stub.returns(expectedResult);
+
+        const runCommand : Function = webExt.cmd[cmd];
+        const result = await runCommand(params, options);
+
+        // Check whether parameters and return values are forwarded as-is.
+        sinon.assert.calledOnce(stub);
+        sinon.assert.calledWithExactly(stub, params, options);
+        assert.equal(expectedResult, result);
+      });
+    }
   });
 });
