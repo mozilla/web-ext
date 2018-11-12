e619d5f3c233939edc22c8ae06ecbaa96551793e
Luca Greco <lgreco@mozilla.com>
2018-11-12 17:42:48 +0100

    chore(travis): Increased mocha timeout to 30000ms for travis windows workers

--- tasks/configs/mochaTest.js
+++ tasks/configs/mochaTest.js
@@ -2,7 +2,7 @@ module.exports = {
   options: {
     // On TravisCI, sometimes the tests that require I/O need extra time
     // (even more when running in a travis windows worker).
-    timeout: process.env.TRAVIS_OS_NAME === 'windows' ? 20000 : 10000,
+    timeout: process.env.TRAVIS_OS_NAME === 'windows' ? 30000 : 10000,
     reporter: 'mocha-multi',
     reporterOptions: {
       spec: '-',
