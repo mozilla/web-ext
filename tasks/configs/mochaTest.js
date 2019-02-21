776964530c4d78fee68920a941e22e69c4466b86
Luca Greco <lgreco@mozilla.com>
2019-02-21 17:50:57 +0100

    chore: Updated mocha to version 6.0.0

--- tasks/configs/mochaTest.js
+++ tasks/configs/mochaTest.js
@@ -3,10 +3,7 @@ module.exports = {
     // On TravisCI, sometimes the tests that require I/O need extra time
     // (even more when running in a travis windows worker).
     timeout: process.env.TRAVIS_OS_NAME === 'windows' ? 30000 : 10000,
-    reporter: 'mocha-multi',
-    reporterOptions: {
-      spec: '-',
-    },
+    reporter: 'spec',
     require: [
       '@babel/register',
       './tests/setup',
