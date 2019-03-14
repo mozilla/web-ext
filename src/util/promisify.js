d62abdb073378f276767be86f70651b8be99a843
Luca Greco <lgreco@mozilla.com>
2019-03-14 13:53:27 +0100

    chore: Applied minor tweaks on the promisify module inline comment

--- src/util/promisify.js
+++ src/util/promisify.js
@@ -1,14 +1,16 @@
 /* @flow */
 
-// A small promisify helper to make it easier to customize a
-// function promisified (using the 'util' module available in
-// nodejs >= 8) to resolve to an array of results:
-//
-//    import {promisify} from 'util';
-//    import {multiArgsPromisedFn} from '../util/promisify';
-//
-//    aCallbackBasedFn[promisify.custom] = multiArgsPromisedFn(tmp.dir);
-//    ...
+/*
+ * A small promisify helper to make it easier to customize a
+ * function promisified (using the 'util' module available in
+ * nodejs >= 8) to resolve to an array of results:
+ *
+ *    import {promisify} from 'util';
+ *    import {multiArgsPromisedFn} from '../util/promisify';
+ *
+ *    aCallbackBasedFn[promisify.custom] = multiArgsPromisedFn(tmp.dir);
+ *    ...
+ */
 export function multiArgsPromisedFn(fn: Function): Function {
   return (...callerArgs: Array<any>): Promise<any> => {
     return new Promise((resolve, reject) => {
