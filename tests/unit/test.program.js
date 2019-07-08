mozilla/web-ext

commit edebef46eb0239a48ae993fd56c19cf8031f1a89

fix: Updated yargs to version 13.2.1 (#1520) (fix #1307)
* fix: Updated yargs to version 13.2.1
* fix: Fixed "Unknown argument: no-config-discovery" issue (#1307).
* chore: Change yargs deprecated demand to demandOption
* chore: Updated inline comment related to issues with yargs package.json config loading

src/program.js
@@ -47,6 +47,7 @@ type ExecuteOptions = {
  * The command line program.
  */
 export class Program {
+  absolutePackageDir: string;
   yargs: any;
   commands: { [key: string]: Function };
   shouldExitProgram: boolean;
@@ -66,13 +67,24 @@ export class Program {
     argv = argv || process.argv.slice(2);
 
     // NOTE: always initialize yargs explicitly with the package dir
-    // so that we are sure that it is going to load the 'boolean-negation: false'
-    // config (See web-ext#469 for rationale).
+    // to avoid side-effects due to yargs looking for its configuration
+    // section from a package.json file stored in an arbitrary directory
+    // (e.g. in tests yargs would end up loading yargs config from the
+    // mocha package.json). web-ext package.json doesn't contain any yargs
+    // section as it is deprecated and we configure yargs using
+    // yargs.parserConfiguration. See web-ext#469 for rationale.
     const yargsInstance = yargs(argv, absolutePackageDir);
 
+    this.absolutePackageDir = absolutePackageDir;
     this.verboseEnabled = false;
     this.shouldExitProgram = true;
     this.yargs = yargsInstance;
+
+    // The following yargs configuration option is needed to fix #304.
+    this.yargs.parserConfiguration({
+      'boolean-negation': false,
+    });
+
     this.yargs.strict();
 
     this.commands = {};
@@ -113,10 +125,10 @@ export class Program {
     this.options = {...this.options, ...options};
     Object.keys(options).forEach((key) => {
       options[key].global = true;
-      if (options[key].demand === undefined) {
+      if (options[key].demandOption === undefined) {
         // By default, all options should be "demanded" otherwise
         // yargs.strict() will think they are missing when declared.
-        options[key].demand = true;
+        options[key].demandOption = true;
       }
     });
     this.yargs.options(options);
@@ -137,7 +149,6 @@ export class Program {
   }
 
   async execute(
-    absolutePackageDir: string,
     {
       checkForUpdates = defaultUpdateChecker,
       systemProcess = process,
@@ -154,9 +165,10 @@ export class Program {
     this.yargs.exitProcess(this.shouldExitProgram);
 
     const argv = this.yargs.argv;
+
     const cmd = argv._[0];
 
-    const version = getVersion(absolutePackageDir);
+    const version = getVersion(this.absolutePackageDir);
     const runCommand = this.commands[cmd];
 
     if (argv.verbose) {
@@ -173,14 +185,15 @@ export class Program {
         throw new UsageError(`Unknown command: ${cmd}`);
       }
       if (globalEnv === 'production') {
-        checkForUpdates ({
-          version: getVersion(absolutePackageDir),
-        });
+        checkForUpdates({version});
       }
 
       const configFiles = [];
 
-      if (argv.configDiscovery) {
+      // Because of an issue with yargs special handling for '--no-' option prefix (See #306)
+      // we need to look explicitly for the options  --config-discovery and --no-config-discovery
+      // (See #1307).
+      if (argv.configDiscovery && !argv.noConfigDiscovery) {
         log.debug(
           'Discovering config files. ' +
           'Set --no-config-discovery to disable');
@@ -283,8 +296,8 @@ export function main(
     runOptions = {},
   }: MainParams = {}
 ): Promise<any> {
-
   const program = new Program(argv, {absolutePackageDir});
+  const version = getVersion(absolutePackageDir);
 
   // yargs uses magic camel case expansion to expose options on the
   // final argv object. For example, the 'artifacts-dir' option is alternatively
@@ -302,7 +315,7 @@ Example: $0 --help run.
     .help('help')
     .alias('h', 'help')
     .env(envPrefix)
-    .version(() => getVersion(absolutePackageDir))
+    .version(version)
     .demandCommand(1, 'You must specify a command')
     .strict();
 
@@ -327,33 +340,35 @@ Example: $0 --help run.
       alias: 'v',
       describe: 'Show verbose output',
       type: 'boolean',
+      demandOption: false,
     },
     'ignore-files': {
       alias: 'i',
       describe: 'A list of glob patterns to define which files should be ' +
                 'ignored. (Example: --ignore-files=path/to/first.js ' +
                 'path/to/second.js "**/*.log")',
-      demand: false,
+      demandOption: false,
       requiresArg: true,
       type: 'array',
     },
     'no-input': {
       describe: 'Disable all features that require standard input',
       type: 'boolean',
+      demandOption: false,
     },
     'config': {
       alias: 'c',
       describe: 'Path to a CommonJS config file to set ' +
         'option defaults',
       default: undefined,
-      demand: false,
+      demandOption: false,
       requiresArg: true,
       type: 'string',
     },
     'config-discovery': {
       describe: 'Discover config files in home directory and ' +
         'working directory. Disable with --no-config-discovery.',
-      demand: false,
+      demandOption: false,
       default: true,
       type: 'boolean',
     },
@@ -380,32 +395,32 @@ Example: $0 --help run.
       commands.sign, {
         'api-key': {
           describe: 'API key (JWT issuer) from addons.mozilla.org',
-          demand: true,
+          demandOption: true,
           type: 'string',
         },
         'api-secret': {
           describe: 'API secret (JWT secret) from addons.mozilla.org',
-          demand: true,
+          demandOption: true,
           type: 'string',
         },
         'api-url-prefix': {
           describe: 'Signing API URL prefix',
           default: 'https://addons.mozilla.org/api/v3',
-          demand: true,
+          demandOption: true,
           type: 'string',
         },
         'api-proxy': {
           describe:
             'Use a proxy to access the signing API. ' +
             'Example: https://yourproxy:6000 ',
-          demand: false,
+          demandOption: false,
           type: 'string',
         },
         'id': {
           describe:
             'A custom ID for the extension. This has no effect if the ' +
             'extension already declares an explicit ID in its manifest.',
-          demand: false,
+          demandOption: false,
           type: 'string',
         },
         'timeout': {
@@ -425,7 +440,7 @@ Example: $0 --help run.
                   'firefox-android). Specify this option multiple times to ' +
                   'run against multiple targets.',
         default: 'firefox-desktop',
-        demand: false,
+        demandOption: false,
         type: 'array',
       },
       'firefox': {
@@ -435,7 +450,7 @@ Example: $0 --help run.
                   'If not specified, the default Firefox will be used. ' +
                   'You can specify the following aliases in lieu of a path: ' +
                   'firefox, beta, nightly, firefoxdeveloperedition.',
-        demand: false,
+        demandOption: false,
         type: 'string',
       },
       'firefox-profile': {
@@ -444,25 +459,25 @@ Example: $0 --help run.
                   'can be specified as a directory or a name, such as one ' +
                   'you would see in the Profile Manager. If not specified, ' +
                   'a new temporary profile will be created.',
-        demand: false,
+        demandOption: false,
         type: 'string',
       },
       'keep-profile-changes': {
         describe: 'Run Firefox directly in custom profile. Any changes to ' +
                   'the profile will be saved.',
-        demand: false,
+        demandOption: false,
         type: 'boolean',
       },
       'no-reload': {
         describe: 'Do not reload the extension when source files change',
-        demand: false,
+        demandOption: false,
         type: 'boolean',
       },
       'pre-install': {
         describe: 'Pre-install the extension into the profile before ' +
                   'startup. This is only needed to support older versions ' +
                   'of Firefox.',
-        demand: false,
+        demandOption: false,
         type: 'boolean',
       },
       'pref': {
@@ -470,7 +485,7 @@ Example: $0 --help run.
                   '(example: --pref=general.useragent.locale=fr-FR). ' +
                   'You can repeat this option to set more than one ' +
                   'preference.',
-        demand: false,
+        demandOption: false,
         requiresArg: true,
         type: 'array',
         coerce: coerceCLICustomPreference,
@@ -478,39 +493,39 @@ Example: $0 --help run.
       'start-url': {
         alias: ['u', 'url'],
         describe: 'Launch firefox at specified page',
-        demand: false,
+        demandOption: false,
         requiresArg: true,
         type: 'array',
       },
       'browser-console': {
         alias: ['bc'],
         describe: 'Open the DevTools Browser Console.',
-        demand: false,
+        demandOption: false,
         type: 'boolean',
       },
       // Firefox for Android CLI options.
       'adb-bin': {
         describe: 'Specify a custom path to the adb binary',
-        demand: false,
+        demandOption: false,
         type: 'string',
         requiresArg: true,
       },
       'adb-host': {
         describe: 'Connect to adb on the specified host',
-        demand: false,
+        demandOption: false,
         type: 'string',
         requiresArg: true,
       },
       'adb-port': {
         describe: 'Connect to adb on the specified port',
-        demand: false,
+        demandOption: false,
         type: 'string',
         requiresArg: true,
       },
       'adb-device': {
         alias: ['android-device'],
         describe: 'Connect to the specified adb device name',
-        demand: false,
+        demandOption: false,
         type: 'string',
         requiresArg: true,
       },
@@ -519,7 +534,7 @@ Example: $0 --help run.
           'Run a specific Firefox for Android APK. ' +
           'Example: org.mozilla.fennec_aurora'
         ),
-        demand: false,
+        demandOption: false,
         type: 'string',
         requiresArg: true,
       },
@@ -564,5 +579,5 @@ Example: $0 --help run.
     .command('docs', 'Open the web-ext documentation in a browser',
              commands.docs, {});
 
-  return program.execute(absolutePackageDir, runOptions);
+  return program.execute({getVersion, ...runOptions});
 }

tests/unit/test.program.js
@@ -30,14 +30,16 @@ describe('program.Program', () => {
   function execProgram(program, options = {}) {
     const fakeProcess = createFakeProcess();
     const absolutePackageDir = path.join(__dirname, '..', '..');
-    return program.execute(
-      absolutePackageDir, {
-        getVersion: () => 'not-a-real-version',
-        checkForUpdates: spy(),
-        systemProcess: fakeProcess,
-        shouldExitProgram: false,
-        ...options,
-      });
+    if (program.absolutePackageDir == null) {
+      program.absolutePackageDir = absolutePackageDir;
+    }
+    return program.execute({
+      getVersion: () => 'not-a-real-version',
+      checkForUpdates: spy(),
+      systemProcess: fakeProcess,
+      shouldExitProgram: false,
+      ...options,
+    });
   }
 
   it('executes a command callback', () => {
@@ -216,6 +218,7 @@ describe('program.Program', () => {
     program.setGlobalOptions({
       verbose: {
         type: 'boolean',
+        demandOption: false,
       },
     });
     return execProgram(program, {logStream})
@@ -236,6 +239,7 @@ describe('program.Program', () => {
     program.setGlobalOptions({
       verbose: {
         type: 'boolean',
+        demandOption: false,
       },
     });
     return execProgram(program)
@@ -326,9 +330,9 @@ describe('program.main', () => {
       projectRoot,
       {
         argv,
+        getVersion: () => 'not-a-real-version',
         runOptions: {
           discoverConfigFiles: async () => [],
-          getVersion: () => 'not-a-real-version',
           checkForUpdates: spy(),
           shouldExitProgram: false,
           systemProcess: createFakeProcess(),
@@ -567,7 +571,7 @@ describe('program.main', () => {
 
     const discoverConfigFiles = sinon.spy(() => Promise.resolve([]));
     await execProgram(
-      ['--no-config-discovery', 'lint'],
+      ['lint', '--no-config-discovery'],
       {
         commands: fakeCommands,
         runOptions: {

tests/unit/test.config.js
@@ -66,7 +66,7 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -83,7 +83,7 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'default/value/option/definition',
           },
         },
@@ -103,7 +103,7 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'default/value/option/definition',
           },
         },
@@ -121,12 +121,12 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'default/value/option/definition',
           },
           'artifacts-dir': {
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -145,12 +145,12 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'default/value/option/definition',
           },
           'artifacts-dir': {
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -169,7 +169,7 @@ describe('config', () => {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             // In the real world this would do something like
             // (sourceDir) => path.resolve(sourceDir)
             coerce,
@@ -189,6 +189,7 @@ describe('config', () => {
         globalOpt: {
           'overwrite-files': {
             type: 'boolean',
+            demandOption: false,
             // No default is set here explicitly but yargs will set it to
             // false implicitly.
           },
@@ -254,7 +255,7 @@ describe('config', () => {
         userCmd: ['fakecommand'],
         globalOpt: {
           'file-path': {
-            demand: false,
+            demandOption: false,
             type: 'string',
           },
         },
@@ -282,7 +283,7 @@ describe('config', () => {
         userCmd: ['fakecommand'],
         globalOpt: {
           'ignore-files': {
-            demand: false,
+            demandOption: false,
             type: 'array',
           },
         },
@@ -302,7 +303,7 @@ describe('config', () => {
            userCmd: ['fakecommand'],
            globalOpt: {
              pref: {
-               demand: false,
+               demandOption: false,
                type: 'array',
              },
            },
@@ -326,6 +327,7 @@ describe('config', () => {
           },
           'verbose': {
             type: 'boolean',
+            demandOption: false,
           },
         },
       });
@@ -376,6 +378,7 @@ describe('config', () => {
           'source-dir': {
             type: 'string',
             default: undefined,
+            demandOption: false,
           },
         },
       });
@@ -391,7 +394,7 @@ describe('config', () => {
         globalOpt: {
           'source-dir': {
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -409,7 +412,7 @@ describe('config', () => {
         globalOpt: {
           'source-dir': {
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -447,7 +450,7 @@ describe('config', () => {
         globalOpt: {
           'source-dir': {
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -486,7 +489,7 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value',
           },
         },
@@ -509,7 +512,7 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value',
           },
         },
@@ -532,7 +535,7 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -551,7 +554,7 @@ describe('config', () => {
         command: 'sign',
         commandOpt: {
           'file-path': {
-            demand: false,
+            demandOption: false,
             type: 'string',
           },
         },
@@ -588,13 +591,13 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiUrl',
           },
         },
@@ -617,13 +620,13 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiUrl',
           },
         },
@@ -645,14 +648,14 @@ describe('config', () => {
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
         globalOpt: {
           'source-dir': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
           },
         },
       });
@@ -702,7 +705,7 @@ describe('config', () => {
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
         },
@@ -726,7 +729,7 @@ describe('config', () => {
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
         },
@@ -750,7 +753,7 @@ describe('config', () => {
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
         },
@@ -775,13 +778,13 @@ describe('config', () => {
           'api-url': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
           'api-key': {
             requiresArg: true,
             type: 'string',
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
         },
@@ -805,7 +808,7 @@ describe('config', () => {
         commandOpt: {
           'api-url': {
             requiresArg: true,
-            demand: false,
+            demandOption: false,
             default: 'pretend-default-value-of-apiKey',
           },
         },
@@ -831,7 +834,7 @@ describe('config', () => {
       program.command('run', 'this is a fake command', sinon.stub(), {
         'no-reload': {
           type: 'boolean',
-          demand: false,
+          demandOption: false,
         },
       });
 
@@ -839,7 +842,7 @@ describe('config', () => {
         'api-url': {
           requiresArg: true,
           type: 'string',
-          demand: false,
+          demandOption: false,
           default: 'pretend-default-value-of-apiKey',
         },
       });

commit 4c60c81546e0315257b70a3c79a09a02b9d6a601

fix: support multiple values after --ignore-files again
The yargs upgrade from 6.6.0 to 13.2.1 in edebef46eb broke the
documented ability to pass multiple files to ignore after the
`--ignore-files` (aka `-i`) parameter.

This is because yargs 11 started to treat `requiresArg` as an implicit
`nargs: 1`, i.e. to accept exactly one value only.

This patch fixes the issue by dropping `requiresArg` and validating the
number of parameters in the `execute` function instead.

src/program.js
@@ -166,6 +166,12 @@ export class Program {
 
     const argv = this.yargs.argv;
 
+    // Replacement for the "requiresArg: true" parameter until the following bug
+    // is fixed: https://github.com/yargs/yargs/issues/1098
+    if (argv.ignoreFiles && !argv.ignoreFiles.length) {
+      throw new UsageError('Not enough arguments following: ignore-files');
+    }
+
     const cmd = argv._[0];
 
     const version = getVersion(this.absolutePackageDir);
@@ -348,7 +354,10 @@ Example: $0 --help run.
                 'ignored. (Example: --ignore-files=path/to/first.js ' +
                 'path/to/second.js "**/*.log")',
       demandOption: false,
-      requiresArg: true,
+      // The following option prevents yargs>=11 from parsing multiple values,
+      // so the minimum value requirement is enforced in execute instead.
+      // Upstream bug: https://github.com/yargs/yargs/issues/1098
+      // requiresArg: true,
       type: 'array',
     },
     'no-input': {

tests/unit/test.program.js
@@ -709,6 +709,30 @@ describe('program.main', () => {
 
     sinon.assert.called(logStream.makeVerbose);
   });
+
+  it('requires a parameter after --ignore-files', async () => {
+    const fakeCommands = fake(commands);
+    return execProgram(['build', '--ignore-files'], {commands: fakeCommands})
+      .then(makeSureItFails())
+      .catch((error) => {
+        assert.match(
+          error.message, /Not enough arguments following: ignore-files/);
+      });
+  });
+
+  it('supports multiple parameters after --ignore-files', async () => {
+    const fakeCommands = fake(commands, {
+      build: () => Promise.resolve(),
+    });
+    return execProgram(
+      ['build', '--ignore-files', 'f1', 'f2', '-a', 'xxx', '-i', 'f4', 'f3'],
+      {commands: fakeCommands})
+      .then(() => {
+        const options = fakeCommands.build.firstCall.args[0];
+        assert.deepEqual(options.ignoreFiles, ['f1', 'f2', 'f4', 'f3']);
+        assert.equal(options.artifactsDir, 'xxx');
+      });
+  });
 });
 
 describe('program.defaultVersionGetter', () => {