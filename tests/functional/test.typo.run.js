2d37deeb881a4b989a4379c5b1a6abc5963270ae
James George <jamesgeorge998001@gmail.com>
2019-11-26 17:01:35 +0530

    feat(cli): suggest matching commands (#1763)

---
+++ tests/functional/test.typo.run.js
@@ -0,0 +1,16 @@
+/* @flow */
+import {describe, it} from 'mocha';
+import {assert} from 'chai';
+
+import {execWebExt} from './common';
+
+describe('web-ext', () => {
+  it('recommends matching command', async () => {
+    const argv = ['buld'];
+
+    return execWebExt(argv, {}).waitForExit.then(({exitCode, stderr}) => {
+      assert.notEqual(exitCode, 0);
+      assert.match(stderr, /Did you mean build/);
+    });
+  });
+});
