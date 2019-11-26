57e726adfa1f8d0db5536c79722dea28d570ae76
jamesgeorge007 <jamesgeorge998001@gmail.com>
2019-11-26 08:51:38 +0530

    test: new test case

---
+++ tests/functional/test.typo.run.js
@@ -0,0 +1,17 @@
+/* @flow */
+import {describe, it} from 'mocha';
+
+import {
+  withTempDir, execWebExt, reportCommandErrors,
+} from './common';
+
+describe('web-ext', () => {
+  it('should not accept: --target INVALIDTARGET', async () => {
+    const argv = ['buld'];
+
+    return execWebExt(argv, {}).waitForExit.then(({exitCode, stderr}) => {
+      assert.notEqual(exitCode, 0);
+      assert.match(stderr, /Did you mean build/);
+    });
+  });
+});
