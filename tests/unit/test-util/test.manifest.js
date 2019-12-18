62ad69dee8911a517acc67407fc9ff277ed675fb
Andy Richardson <andy.john.richardson@gmail.com>
2019-12-18 17:03:42 +0000

    Fix linting in non-src folder

--- tests/unit/test-util/test.manifest.js
+++ tests/unit/test-util/test.manifest.js
@@ -1,48 +1,48 @@
 /* @flow */
-import path from "path";
+import path from 'path';
 
-import { describe, it } from "mocha";
-import { assert } from "chai";
-import deepcopy from "deepcopy";
-import { fs } from "mz";
+import { describe, it } from 'mocha';
+import { assert } from 'chai';
+import deepcopy from 'deepcopy';
+import { fs } from 'mz';
 
-import { onlyInstancesOf, InvalidManifest } from "../../../src/errors";
+import { onlyInstancesOf, InvalidManifest } from '../../../src/errors';
 import getValidatedManifest, {
-  getManifestId
-} from "../../../src/util/manifest";
-import { withTempDir } from "../../../src/util/temp-dir";
+  getManifestId,
+} from '../../../src/util/manifest';
+import { withTempDir } from '../../../src/util/temp-dir';
 import {
   basicManifest,
   makeSureItFails,
-  manifestWithoutApps
-} from "../helpers";
+  manifestWithoutApps,
+} from '../helpers';
 
-describe("util/manifest", () => {
-  describe("getValidatedManifest", () => {
-    it("returns a valid manifest", () =>
-      withTempDir(tmpDir =>
+describe('util/manifest', () => {
+  describe('getValidatedManifest', () => {
+    it('returns a valid manifest', () =>
+      withTempDir((tmpDir) =>
         writeManifest(tmpDir.path(), basicManifest)
           .then(() => getValidatedManifest(tmpDir.path()))
-          .then(manifestData => {
+          .then((manifestData) => {
             assert.deepEqual(manifestData, basicManifest);
           })
       ));
 
-    it("allows manifests without an applications property", () =>
-      withTempDir(tmpDir =>
+    it('allows manifests without an applications property', () =>
+      withTempDir((tmpDir) =>
         writeManifest(tmpDir.path(), manifestWithoutApps)
           .then(() => getValidatedManifest(tmpDir.path()))
-          .then(manifestData => {
+          .then((manifestData) => {
             assert.deepEqual(manifestData, manifestWithoutApps);
           })
       ));
 
-    it("reports an error for a missing manifest file", () => {
-      const nonExistentDir = path.join("dev", "null", "nowhere");
+    it('reports an error for a missing manifest file', () => {
+      const nonExistentDir = path.join('dev', 'null', 'nowhere');
       return getValidatedManifest(nonExistentDir)
         .then(makeSureItFails())
         .catch(
-          onlyInstancesOf(InvalidManifest, error => {
+          onlyInstancesOf(InvalidManifest, (error) => {
             assert.match(error.message, /Could not read manifest\.json/);
             // Make sure the filename is included in the exception message.
             // This is actually done by default in file system error messages.
@@ -51,43 +51,43 @@ describe("util/manifest", () => {
         );
     });
 
-    it("reports an error for invalid manifest JSON", () =>
-      withTempDir(tmpDir => {
+    it('reports an error for invalid manifest JSON', () =>
+      withTempDir((tmpDir) => {
         const badManifest = `{
           "name": "I'm an invalid JSON Manifest",,
           "version": "0.0.0"
         }`;
-        const manifestFile = path.join(tmpDir.path(), "manifest.json");
+        const manifestFile = path.join(tmpDir.path(), 'manifest.json');
         return fs
           .writeFile(manifestFile, badManifest)
           .then(() => getValidatedManifest(tmpDir.path()))
           .then(makeSureItFails())
           .catch(
-            onlyInstancesOf(InvalidManifest, error => {
+            onlyInstancesOf(InvalidManifest, (error) => {
               assert.match(
                 error.message,
                 /Error parsing manifest\.json file at /
               );
               assert.include(
                 error.message,
-                "Unexpected token , in JSON at position 51"
+                'Unexpected token , in JSON at position 51'
               );
               assert.include(error.message, manifestFile);
             })
           );
       }));
 
-    it("reports an error when missing a name", () =>
-      withTempDir(tmpDir => {
+    it('reports an error when missing a name', () =>
+      withTempDir((tmpDir) => {
         const noNameManifest = deepcopy(basicManifest);
         delete noNameManifest.name;
 
         return writeManifest(tmpDir.path(), noNameManifest).then(
-          manifestFile => {
+          (manifestFile) => {
             return getValidatedManifest(tmpDir.path())
               .then(makeSureItFails())
               .catch(
-                onlyInstancesOf(InvalidManifest, error => {
+                onlyInstancesOf(InvalidManifest, (error) => {
                   assert.match(
                     error.message,
                     /Manifest at .* is invalid: missing "name" property/
@@ -99,17 +99,17 @@ describe("util/manifest", () => {
         );
       }));
 
-    it("reports an error when missing version", () =>
-      withTempDir(tmpDir => {
+    it('reports an error when missing version', () =>
+      withTempDir((tmpDir) => {
         const noVersionManifest = deepcopy(basicManifest);
         delete noVersionManifest.version;
 
         return writeManifest(tmpDir.path(), noVersionManifest).then(
-          manifestFile => {
+          (manifestFile) => {
             return getValidatedManifest(tmpDir.path())
               .then(makeSureItFails())
               .catch(
-                onlyInstancesOf(InvalidManifest, error => {
+                onlyInstancesOf(InvalidManifest, (error) => {
                   assert.match(
                     error.message,
                     /Manifest at .* is invalid: missing "version" property/
@@ -121,17 +121,17 @@ describe("util/manifest", () => {
         );
       }));
 
-    it("reports an error when missing applications.gecko", () =>
-      withTempDir(tmpDir => {
+    it('reports an error when missing applications.gecko', () =>
+      withTempDir((tmpDir) => {
         const incompleteManifest = deepcopy(basicManifest);
         delete incompleteManifest.applications.gecko;
 
         return writeManifest(tmpDir.path(), incompleteManifest).then(
-          manifestFile => {
+          (manifestFile) => {
             return getValidatedManifest(tmpDir.path())
               .then(makeSureItFails())
               .catch(
-                onlyInstancesOf(InvalidManifest, error => {
+                onlyInstancesOf(InvalidManifest, (error) => {
                   assert.match(
                     error.message,
                     /Manifest at .* is invalid: missing "applications.gecko".*/
@@ -143,20 +143,20 @@ describe("util/manifest", () => {
         );
       }));
 
-    it("allows a missing applications.gecko.id", () =>
-      withTempDir(tmpDir => {
+    it('allows a missing applications.gecko.id', () =>
+      withTempDir((tmpDir) => {
         const incompleteManifest = deepcopy(basicManifest);
         delete incompleteManifest.applications.gecko.id;
 
         return writeManifest(tmpDir.path(), incompleteManifest)
           .then(() => getValidatedManifest(tmpDir.path()))
-          .then(manifestData => {
+          .then((manifestData) => {
             assert.strictEqual(getManifestId(manifestData), undefined);
           });
       }));
 
-    it("concatenates errors in error message", () =>
-      withTempDir(tmpDir => {
+    it('concatenates errors in error message', () =>
+      withTempDir((tmpDir) => {
         const manifestWithErrors = deepcopy(basicManifest);
         delete manifestWithErrors.name;
         delete manifestWithErrors.version;
@@ -165,7 +165,7 @@ describe("util/manifest", () => {
           return getValidatedManifest(tmpDir.path())
             .then(makeSureItFails())
             .catch(
-              onlyInstancesOf(InvalidManifest, error => {
+              onlyInstancesOf(InvalidManifest, (error) => {
                 assert.match(
                   error.message,
                   /missing "name" property; missing "version" property/
@@ -175,37 +175,37 @@ describe("util/manifest", () => {
         });
       }));
 
-    it("ignore UTF-8 BOM in manifest JSON", () =>
-      withTempDir(async tmpDir => {
+    it('ignore UTF-8 BOM in manifest JSON', () =>
+      withTempDir(async (tmpDir) => {
         const manifestDataWithBOM = `\uFEFF${JSON.stringify(basicManifest)}`;
-        const manifestFile = path.join(tmpDir.path(), "manifest.json");
+        const manifestFile = path.join(tmpDir.path(), 'manifest.json');
         await fs.writeFile(manifestFile, manifestDataWithBOM);
         const manifestData = await getValidatedManifest(tmpDir.path());
 
         assert.deepEqual(manifestData, basicManifest);
       }));
 
-    it("allows comments in manifest JSON", () =>
-      withTempDir(async tmpDir => {
+    it('allows comments in manifest JSON', () =>
+      withTempDir(async (tmpDir) => {
         const manifestWithComments = `{
           "name": "the extension",
           "version": "0.0.1" // comments
         }`;
-        const manifestFile = path.join(tmpDir.path(), "manifest.json");
+        const manifestFile = path.join(tmpDir.path(), 'manifest.json');
         await fs.writeFile(manifestFile, manifestWithComments);
         const manifestData = await getValidatedManifest(tmpDir.path());
 
         assert.deepEqual(manifestData, manifestWithoutApps);
       }));
 
-    it("reports an error with line number in manifest JSON with comments", () =>
-      withTempDir(async tmpDir => {
+    it('reports an error with line number in manifest JSON with comments', () =>
+      withTempDir(async (tmpDir) => {
         const invalidManifestWithComments = `{
           // a comment in its own line
           // another comment on its own line
           "name": "I'm an invalid JSON Manifest
         }`;
-        const manifestFile = path.join(tmpDir.path(), "manifest.json");
+        const manifestFile = path.join(tmpDir.path(), 'manifest.json');
         await fs.writeFile(manifestFile, invalidManifestWithComments);
         const promise = getValidatedManifest(tmpDir.path());
 
@@ -218,39 +218,39 @@ describe("util/manifest", () => {
       }));
   });
 
-  describe("getManifestId", () => {
-    const id = "basic-manifest@web-ext-test-suite";
+  describe('getManifestId', () => {
+    const id = 'basic-manifest@web-ext-test-suite';
 
-    it("returns application gecko ID (application.gecko.id)", () => {
+    it('returns application gecko ID (application.gecko.id)', () => {
       assert.equal(getManifestId(basicManifest), id);
     });
 
-    it("returns browser specific gecko ID (browser_specific_settings.gecko.id)", () => {
+    it('returns browser specific gecko ID (browser_specific_settings.gecko.id)', () => {
       assert.equal(
         getManifestId({
           ...basicManifest,
           applications: undefined,
           browser_specific_settings: {
             gecko: {
-              id
-            }
-          }
+              id,
+            },
+          },
         }),
         id
       );
     });
 
-    it("returns undefined when gecko does not exist", () => {
+    it('returns undefined when gecko does not exist', () => {
       assert.equal(
         getManifestId({
           ...basicManifest,
-          applications: {}
+          applications: {},
         }),
         id
       );
     });
 
-    it("returns undefined when ID is not specified", () => {
+    it('returns undefined when ID is not specified', () => {
       assert.strictEqual(
         getManifestId({ ...basicManifest, applications: undefined }),
         undefined
@@ -262,7 +262,7 @@ describe("util/manifest", () => {
 });
 
 function writeManifest(destDir, manifestData) {
-  const manifestFile = path.join(destDir, "manifest.json");
+  const manifestFile = path.join(destDir, 'manifest.json');
   return fs
     .writeFile(manifestFile, JSON.stringify(manifestData))
     .then(() => manifestFile);
