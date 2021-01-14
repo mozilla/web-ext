2d8e5a998ba1e73b90535793dc193ddd5013ac06
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2021-01-15 00:33:09 +0530

    added tests to test out new adb util export

--- tests/fixtures/import-as-esm/test-import.mjs
+++ tests/fixtures/import-as-esm/test-import.mjs
@@ -4,4 +4,9 @@ import assert from 'assert';
 import webExt from 'web-ext';
 
 assert.deepEqual(Object.keys(webExt).sort(), ['cmd', 'main', 'util'].sort());
+assert.deepEqual(Object.keys(webExt.util).sort(), ['logger', 'adb'].sort());
+assert.deepEqual(
+  Object.keys(webExt.util.adb).sort(),
+  ['listADBDevices', 'listADBFirefoxAPKs'].sort(),
+);
 assert.equal(typeof webExt.cmd.run, 'function');
