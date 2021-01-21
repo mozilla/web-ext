49f1e7175c3fb37b4fae7507afafe6bf108fb8f1
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2021-01-21 09:05:57 +0530

    testing new exposed adb functions

--- tests/unit/test-util/test.adb.js
+++ tests/unit/test-util/test.adb.js
@@ -13,6 +13,7 @@ import {
 import ADBUtils, {
   ARTIFACTS_DIR_PREFIX,
   DEVICE_DIR_BASE,
+  listADBDevices, listADBFirefoxAPKs,
 } from '../../../src/util/adb';
 import {
   consoleStream, // instance is imported to inspect logged messages
@@ -1307,6 +1308,34 @@ describe('utils/adb', () => {
         adb.fakeADBClient.forward,
         'device1', 'local:fake', 'remote:fake'
       );
+    });
+  });
+
+  describe('test exposed adb API functions', () => {
+    it('test listADBDevices', async () => {
+      const stubDiscoverDevices = sinon.stub(
+        ADBUtils.prototype, 'discoverDevices'
+      );
+      stubDiscoverDevices.resolves(['emulator1', 'device2']);
+      const promise = listADBDevices();
+      const devices = await assert.isFulfilled(promise);
+      assert.deepEqual(devices, ['emulator1', 'device2']);
+    });
+
+    it('test listADBFirefoxAPKs', async () => {
+      const adb = getFakeADBKit({});
+      const stubDiscoverInstalledFirefoxAPKs = sinon.stub(
+        ADBUtils.prototype, 'discoverInstalledFirefoxAPKs'
+      );
+      stubDiscoverInstalledFirefoxAPKs
+        .resolves(['package1', 'package2', 'package3']);
+      const promise = listADBFirefoxAPKs('device1');
+      const packages = await assert.isFulfilled(promise);
+      sinon.assert.calledWith(
+        new ADBUtils({adb}).discoverInstalledFirefoxAPKs,
+        'device1'
+      );
+      assert.deepEqual(packages, ['package1', 'package2', 'package3']);
     });
   });
 
