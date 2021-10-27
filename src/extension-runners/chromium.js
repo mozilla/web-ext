fd80612bfece83cf4b8bfea3f2f00a25124806e4
eight04 <eight04@gmail.com>
2021-10-27 14:31:44 +0800

    fix: replace localhost with ipv4 to avoid ipv6 issue

--- src/extension-runners/chromium.js
+++ src/extension-runners/chromium.js
@@ -136,7 +136,7 @@ export class ChromiumExtensionRunner {
     // Start a websocket server on a free localhost TCP port.
     this.wss = await new Promise((resolve) => {
       const server = new WebSocket.Server(
-        {port: 0, host: 'localhost'},
+        {port: 0, host: '127.0.0.1'},
         // Wait the server to be listening (so that the extension
         // runner can successfully retrieve server address and port).
         () => resolve(server));
