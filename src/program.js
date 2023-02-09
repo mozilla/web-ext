b678141b45de780c052dcf8f3b1ebe68234351d4
Rob Wu <rob@robwu.nl>
2023-02-09 16:20:21 +0100

    docs: document "deved" alias for "firefoxdeveloperedition" (#2637)
    This documents the "deved" alias introduced in #1906, which was released
    in 4.3.0.

--- src/program.js
+++ src/program.js
@@ -653,7 +653,7 @@ Example: $0 --help run.
           'or firefox.exe. ' +
           'If not specified, the default Firefox will be used. ' +
           'You can specify the following aliases in lieu of a path: ' +
-          'firefox, beta, nightly, firefoxdeveloperedition. ' +
+          'firefox, beta, nightly, firefoxdeveloperedition (or deved). ' +
           'For Flatpak, use `flatpak:org.mozilla.firefox` where ' +
           '`org.mozilla.firefox` is the application ID.',
         demandOption: false,
